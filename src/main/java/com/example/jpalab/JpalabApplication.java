55df0c7927e004a2d61ccfa06c349813c6099063
omarkhairy429 <omar.fayed.5832@gmail.com>
2026-07-22 20:37:49 +0300

    Query methods for Intern

--- src/main/java/com/example/jpalab/JpalabApplication.java
+++ src/main/java/com/example/jpalab/JpalabApplication.java
@@ -1,13 +1,35 @@
 package com.example.jpalab;
 
+import com.example.jpalab.entity.Intern;
+import com.example.jpalab.repository.InternRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication
 public class JpalabApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpalabApplication.class, args);
+
+		System.out.println();
+	}
+
+	@Bean
+	public CommandLineRunner testingOurRepos(InternRepository internRepository) {
+		return args -> {
+			List<Intern> interns = internRepository.findByInternBirthDateBefore(LocalDate.of(2003, 01, 01));
+			for (Intern intern: interns) {
+				System.out.println(intern.getInternEmail());
+				System.out.println(intern.getInternBirthDate());
+			}
+			Intern intern1 = internRepository.findByInternEmail("omar@example.com");
+			System.out.println(intern1.getInternName());
+		};
 	}
 
 }
--- src/main/java/com/example/jpalab/repository/InternRepository.java
+++ src/main/java/com/example/jpalab/repository/InternRepository.java
@@ -4,7 +4,15 @@ import com.example.jpalab.entity.Intern;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface InternRepository extends JpaRepository<Intern, Integer> {
-
+    public Intern findByInternEmail(String internEmail);
+    public List<Intern> findByTrackTrackName(String trackName);
+    public List<Intern> findByMentorIsNull();
+    public List<Intern> findByMentorMentorName(String mentorName);
+    public List<Intern> findByInternBirthDateAfter(LocalDate date);
+    public List<Intern> findByInternBirthDateBefore(LocalDate date);
 }
