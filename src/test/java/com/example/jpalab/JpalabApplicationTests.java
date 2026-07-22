937efc07ffb3954f60a17f3801eb9ccc8ecb7de7
omarkhairy429 <omar.fayed.5832@gmail.com>
2026-07-22 16:23:51 +0300

    Initializing the project, creating the folder sturcture and application.yaml

---
+++ src/test/java/com/example/jpalab/JpalabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpalabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
