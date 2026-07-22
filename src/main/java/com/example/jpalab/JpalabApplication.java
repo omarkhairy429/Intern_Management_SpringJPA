dee0b0cd2f398f3b14fbeae51c3103f5d62623f9
omarkhairy429 <omar.fayed.5832@gmail.com>
2026-07-22 21:41:00 +0300

    Query Methods for Project

--- src/main/java/com/example/jpalab/JpalabApplication.java
+++ src/main/java/com/example/jpalab/JpalabApplication.java
@@ -1,7 +1,10 @@
 package com.example.jpalab;
 
 import com.example.jpalab.entity.Intern;
+import com.example.jpalab.entity.Project;
+import com.example.jpalab.enums.ProjectStatus;
 import com.example.jpalab.repository.InternRepository;
+import com.example.jpalab.repository.ProjectRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,7 +23,8 @@ public class JpalabApplication {
 	}
 
 	@Bean
-	public CommandLineRunner testingOurRepos(InternRepository internRepository) {
+	public CommandLineRunner testingOurRepos(InternRepository internRepository,
+											 ProjectRepository projectRepository) {
 		return args -> {
 			List<Intern> interns = internRepository.findByInternBirthDateBefore(LocalDate.of(2003, 01, 01));
 			for (Intern intern: interns) {
@@ -29,6 +33,15 @@ public class JpalabApplication {
 			}
 			Intern intern1 = internRepository.findByInternEmail("omar@example.com");
 			System.out.println(intern1.getInternName());
+
+			List<Project> projects = projectRepository.findByProjectName("Task Tracker API");
+			for (Project project: projects) {
+				System.out.println(project.getProjectName());
+				System.out.println(project.getProjectId());
+			}
+			int numberOfPlannedProjects = projectRepository.countByProjectStatus(ProjectStatus.planned);
+			System.out.println("Number of planned projects: "+ numberOfPlannedProjects);
+
 		};
 	}
 
--- src/main/java/com/example/jpalab/repository/ProjectRepository.java
+++ src/main/java/com/example/jpalab/repository/ProjectRepository.java
@@ -1,10 +1,15 @@
 package com.example.jpalab.repository;
 
 import com.example.jpalab.entity.Project;
+import com.example.jpalab.enums.ProjectStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
-
+    public List<Project> findByProjectStatus(ProjectStatus projectStatus);
+    public List<Project> findByProjectName(String projectName);
+    public Integer countByProjectStatus(ProjectStatus projectStatus);
 }
